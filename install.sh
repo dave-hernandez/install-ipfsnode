7da3cc9d65fda8d265e0007eeb13a5b2c8f4c1c0
dave-hernandez <dhernandez@tuta.io>
2015-11-23 01:15:39 -0800

    Brief Outline
    Missing all the important stuff. Will fill in later.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#  IPFS has a very easy setup process, consisting of two basic steps. First, the download of
+#  the latest version from gobuilder, extraction, and then placing the binary in the /usr/local/bin
+#  directory. However, this only makes IPFS accessible from the local machine. For other devices
+#  to be able to take advantage of the web interface and make IPFS browsing easy from any computer
+#  on the LAN, additional steps are needed. The goal of this is to make setup of such a node as
+#  simple as possible.
+
+#  BUILT FOR CENTOS 7 x64
+
+wget https://gobuilder.me/get/github.com/ipfs/go-ipfs/cmd/ipfs/ipfs_master_linux-amd64.zip
+unzip ipfs_master_linux-amd64.zip
+cp ipfs/ipfs /usr/local/bin/ipfs
+
+firewall-cmd --zone=public --add-port=5001/tcp --permanent
+systemctl restart firewalld
+
+ipfs init
+
+ipfs daemon
